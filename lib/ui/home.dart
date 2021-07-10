dee53ca50fcb0fb244e7641e48cd736f5a48637e
Elvis <e.igiebor@cyberticks.com>
2021-07-10 09:43:04 +0100

    added more quotes

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -17,7 +17,10 @@ class _WisdomState extends State<Wisdom> {
     "The fool doth think he is wise, but the wise man knows himself to be a fool",
     "Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect",
      " Always Do What You Are Afraid To Do",
-     "The best way out is always through"
+     "The best way out is always through","When someone loves you, the way they talk about you is different. You feel safe and comfortable.",
+     "Knowing yourself is the beginning of all wisdom.","The only true wisdom is in knowing you know nothing.",
+     "The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.",
+     "Count your age by friends, not years. Count your life by smiles, not tears.","Hold fast to dreams"
   ];
   @override
   Widget build(BuildContext context) {
@@ -25,10 +28,38 @@ class _WisdomState extends State<Wisdom> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(quotes[_index % quotes.length]),
-          FlatButton.icon(onPressed: _showQuote,
-            icon: Icon(Icons.wb_sunny),
-            label: Text("Inspire Me"),)
+          Expanded(
+            child: Center(
+              child: Container(
+                  height: 200,
+                  width: 350,
+                  margin: EdgeInsets.all(30.0),
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(14.5),
+                  ),
+                  child: Center(child: Text(quotes[_index % quotes.length],style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 16.5,
+                  fontStyle: FontStyle.italic,),
+                  )
+                    ,)
+              ),
+            ),
+          ),
+          Divider(thickness: 1.3,),
+          Padding(
+            padding: const EdgeInsets.only(top: 18.0),
+            child: FlatButton.icon(onPressed: _showQuote,
+              color: Colors.greenAccent.shade700,
+              icon: Icon(Icons.wb_sunny),
+              label: Text("Inspire Me",
+                style: TextStyle(color: Colors.white,
+                fontSize: 18.8,),
+              ),
+            ),
+          ),
+          Spacer(),
         ],
       ),
     );
